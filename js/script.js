9a3802610cba9cb8ad3c65f7347d4e72c0e273d9
Vlad <jakubets.vlad@gmail.com>
2020-02-13 22:18:02 +0200

    responsive home page
    need some changes

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ $("document").ready(function () {
     $(".product__btn").on("click", function () {
         $(".popup-window").addClass("popup-window-active");
     });
+    $(".close").on("click", function () {
+        $(".popup-window").removeClass("popup-window-active");
+    });
+    $(".continue").on("click", function () {
+        $(".popup-window").removeClass("popup-window-active");
+    });
+    $(".popup-window").on("click", function () {
+        $(".popup-window").removeClass("popup-window-active");
+    });
 });
